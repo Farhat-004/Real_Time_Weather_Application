5bdfad76e85fd9a35619c0d58e6de36eaa665f9e
Farhatul Hassan <farhatulhassan1@gmail.com>
2025-07-05 09:56:22 +0600

    Vite + React project template with Tailwind V4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <h1 className="bg-gray-500 text-2xl m-auto text-green-300">
+            Vite + React project template with Tailwind V4
+        </h1>
+    );
+}
+
+export default App;
